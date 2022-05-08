e9e4134cf5b47a374f31b26621513fc6e7a26fe5
FangYangLin <74218951+FangYangLin@users.noreply.github.com>
2022-05-08 20:40:23 +0800

    Create updated .js

---
+++ updated .js
@@ -0,0 +1,17 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let earlyRegistrant = false;
+let age = 23;
+
+if(age > 18 && earlyRegistrant === true {
+   raceNumber += 1000;
+}
+
+if(age > 18 && earlyRegistrant === true){
+    console.log(`You will race at 9:30am. Your race number is: ${raceNumber}`);
+} else if (age > 18 && earlyReigistrant === false){
+    console.log(`You will race at 11:00am. Your race number is: ${raceNumber}`);
+} else if (age < 18){
+  console.log(`You will race at 12:30pm. Your race number is: ${raceNumber}`);
+}
+
