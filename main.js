de8cc0bab72f88943f94c790ebdad00fb5c2e9ac
Yang Lin <97666098+ylf13@users.noreply.github.com>
2022-05-08 20:52:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-// Race Day
-
 let raceNumber = Math.floor(Math.random() * 1000);
 
-// check if runner registered early or not by assigning a boolean value 
-var earlyRegistrant = true;
+let earlyRegistrant = false;
+let age = 23;
 
-// Runner's age
-var age = 23
-
-// check if runner is an adult AND registered early 
-// Add 1000 to their raceNumber if this is true
-
-if (age > 18 && earlyRegistrant){
-    raceNumber += 1000;
+if(age > 18 && earlyRegistrant === true {
+   raceNumber += 1000;
 }
 
-// Runners over 18 who registered early
-if(age > 18 && earlyRegistrant){
+if(age > 18 && earlyRegistrant === true){
     console.log(`You will race at 9:30am. Your race number is: ${raceNumber}`);
-}
-
-// Runners over 18 and did not register early
-else if (age > 18 && !earlyReigistrant){
+} else if (age > 18 && earlyReigistrant === false){
     console.log(`You will race at 11:00am. Your race number is: ${raceNumber}`);
-}
-
-// Runners under 18
-else (age < 18){
-    console.log(`You will race at 12:30pm. Your race number is: ${raceNumber}`;)
+} else if (age < 18){
+  console.log(`You will race at 12:30pm. Your race number is: ${raceNumber}`);
 }
